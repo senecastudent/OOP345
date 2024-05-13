5afec53e2a7c71128fa6a4ee04a4ca4887010988
Divyanshu Dugar <divyu2004@gmail.com>
2024-05-13 18:40:53 -0400

    added v1.3 for w1

--- Workshops/WS01/part 1/w1_p1.cpp
+++ Workshops/WS01/part 1/w1_p1.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* argv[])
 	//   ...
 	
 	for (int i = 0; i < argc; i++) {
-		std::cout << "  " << i << ": " << argv[i] << std::endl;
+		std::cout << "  " << i+1 << ": " << argv[i] << std::endl;
 }
 
 	std::cout << "--------------------------\n\n";
