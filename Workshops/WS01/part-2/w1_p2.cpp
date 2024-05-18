f3fef64adfd518ea121dc4936b6aa33450fae8df
Divyanshu Dugar <divyu2004@gmail.com>
2024-05-17 20:38:12 -0400

    added final code-3

--- Workshops/WS01/part-2/w1_p2.cpp
+++ Workshops/WS01/part-2/w1_p2.cpp
@@ -1,3 +1,16 @@
+/*/////////////////////////////////////////////////////////////////////////
+						Workshop-1 Part-2
+Full Name  : Divyanshu Dugar
+Student ID#: 177110210
+Email      : ddugar@myseneca.ca
+Section    : ZAA
+Date       : 17/05/2024
+
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+/////////////////////////////////////////////////////////////////////////*/
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
